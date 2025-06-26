ad899b620337706c9f310056e68ae7ebe9092425
OrlandoDev17 <orlandodev1707@gmail.com>
2025-06-26 15:11:48 -0400

    añadir contador

--- src/components/GalleryGrid.jsx
+++ src/components/GalleryGrid.jsx
@@ -1,4 +1,5 @@
 import "../styles/GalleryGrid.css";
+import GalleryItem from "./GalleryItem";
 
 const GALLERY_ITEMS = [
   {
@@ -69,36 +70,32 @@ const GALLERY_ITEMS = [
 
 export default function GalleryGrid() {
   return (
-    <section>
+    <section className="gallery-section">
       <ul className="gallery-container">
         {GALLERY_ITEMS.map(({ id, title, by, tag, likes, image }) => (
-          <li key={id}>
-            <img src={image} alt={title} />
-            <div className="overlay">
-              <div className="text-ctn">
-                <h3>{title}</h3>
-                <span>Por {by}</span>
-                <div className="tag-ctn">
-                  <span className="tag">{tag}</span>
-                  <span className="likes">
-                    {likes}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="#d32323"
-                      className="icon icon-tabler icons-tabler-filled icon-tabler-heart"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
-                    </svg>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </li>
+          <GalleryItem
+            key={id}
+            id={id}
+            title={title}
+            by={by}
+            tag={tag}
+            likes={likes}
+            image={image}
+          />
         ))}
+      </ul>
+      <ul className="gallery-info">
+        <li>
+          {GALLERY_ITEMS.length} <span>Imagenes</span>
+        </li>
+        <hr />
+        <li>
+          4 <span>Categorias</span>
+        </li>
+        <hr />
+        <li>
+          {GALLERY_ITEMS.length} <span>Mostrando</span>
+        </li>
       </ul>
     </section>
   );
