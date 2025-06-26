6d695637403177451c3dd85667b8a1e79af26787
OrlandoDev17 <orlandodev1707@gmail.com>
2025-06-26 15:59:22 -0400

    crear rutas e instalar dependencias

---
+++ src/components/ImageDetail.jsx
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router-dom';
+import { GALLERY_ITEMS } from '../constants/galleryItems';
+
+const ImageDetail = () => {
+  const { id } = useParams();
+  const image = GALLERY_ITEMS.find(item => item.id === parseInt(id));
+
+  if (!image) {
+    return <div>Imagen no encontrada</div>;
+  }
+
+  return (
+    <div className="image-detail">
+      <img src={image.image} alt={image.title} />
+      <div className="image-info">
+        <h2>{image.title}</h2>
+        <p>Por: {image.by}</p>
+        <p>Categoría: {image.tag}</p>
+        <p>Likes: {image.likes}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ImageDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import ImageDetail from "./components/ImageDetail";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/image/:id" element={<ImageDetail />} />
+    </Routes>
+  </BrowserRouter>
 );
